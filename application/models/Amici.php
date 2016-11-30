a48815934f205dd6c3773bf5b13fd7fc0b89a956
Nicola <nicolafioranelli@gmail.com>
2016-11-30 21:40:24 +0100

    Com. 30/11 sera
    USER
    * implementata richiesta d'amicizia
    * aggiunta navigazione tra blog di utenti
    * aggiunto nuovo post in blog diversi
    * aggiunta vista tutti gli amici
    * implementata cancellazione amico
    * riorganizzate navbar in base a livelli di accesso

--- application/models/Amici.php
+++ application/models/Amici.php
@@ -10,9 +10,9 @@ class Application_Model_Amici
         $this->tabella = new Application_Model_DbTable_Amici();
     }
 
-    public function inserisciAmici()
+    public function inserisciAmici($dati)
     {
-
+        return $this->tabella->insert($dati);
     }
 
     public function aggiornaAmici($dati, $id)
@@ -20,19 +20,30 @@ class Application_Model_Amici
         return $this->tabella->update($dati, "id_amici = '$id'");
     }
 
-    public function eliminaAmici()
+    public function eliminaAmici($id)
     {
-
+        return $this->tabella->delete("richiedente = '$id'");
     }
 
-    public function elencoAmici()
+    public function elencoAmici($id)
     {
-
+        return $this->tabella->fetchAll($this->tabella->select()->where("ricevente =  ? ", $id)->where("stato = 'accepted'"));
     }
 
     public function elencoAmiciById($id)
     {
-        return $this->tabella->fetchAll($this->tabella->select()->where("ricevente =  ? ",$id)->where("stato = 'standby'"));
+        return $this->tabella->fetchAll($this->tabella->select()->where("ricevente =  ? ", $id)->where("stato = 'standby'"));
+    }
+
+    public function elencoRichiestaPresente($idricevente, $idrichiedente)
+    {
+        $risultato = $this->tabella->fetchAll($this->tabella->select()->where("ricevente =  ? ", $idricevente)->where("richiedente = ? ", $idrichiedente));
+        $rowCount = count($risultato);
+        if ($rowCount > 0)
+            return true;
+        else
+            return false;
+
     }
 
 }
